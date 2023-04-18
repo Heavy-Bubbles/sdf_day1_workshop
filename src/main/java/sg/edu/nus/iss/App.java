6a6235775e777ea1a515970630137d6afbe5353d
Heavy-Bubbles <chelseatanxuanlin@gmail.com>
2023-04-18 12:59:11 +0800

    day2 break

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        ArrayList<String> cart = new ArrayList<String>();
+
+        Console c = System.console();
+        String input = c.readLine("Welcome to your shopping cart!");
+
+
+        if (input.equals("list")) {
+            if (cart.size() < 1){
+                System.out.println("Your cart is empty!");
+            } else {
+                System.out.println(cart);
+            }
+        } else {
+            System.err.println("Error! Please type 'list'.");
+    }
     }
 }
+
